13ca95dbb1dc0da6b89dd11b8ce97c027e267880
Deniau Guillaume <gdeniau@SA203-07A.ensi-bourges.fr>
2017-04-11 16:04:21 +0200

    Code comprenant l'affichage du max

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,13 +2,34 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import java.util.List;
+import java.io.*;
+import com.opencsv.*;
 
 public class Hello {
     public static void main(String[] args) {
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
+                                     + " : " + WordUtils.capitalizeFully(message));
+	int monmax =0;
+	try{
+		CSVReader reader = new CSVReader(new FileReader("data.csv"));
+		List<String[]> myEntries = reader.readAll();
+		for (String[] ligne : myEntries){
+			for(String element : ligne){
+				int intElem = Integer.parseInt(element);
+				System.out.println("Nombre lu : "+ element);
+				if(intElem>monmax){
+					monmax = intElem;
+				}
+			}
+		}
+		System.out.println("Maximum : "+ monmax);
+	}
+	catch (IOException e){
+		System.out.println("Problème de lecture de fichiers");
+	}
     }
 }
         
